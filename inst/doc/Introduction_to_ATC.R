7e1d3a1dc24409c3e751aec006e2f4a2c3686d1f
Xiaozhou <xiaozhou.wang@duke.edu>
2018-01-18 23:17:12 -0500

    change vignettes

--- inst/doc/Introduction_to_ATC.R
+++ inst/doc/Introduction_to_ATC.R
@@ -1,1 +1,23 @@
+## ----message=FALSE, warning=FALSE----------------------------------------
+knitr::opts_chunk$set(collapse = T, comment = "#>")
+options(tibble.print_min = 4L, tibble.print_max = 4L)
+library("devtools")
+library(repmis)
+library(dplyr)
+install_github('xiaozhouw/ATC')
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+test_med = "fentanyl (pf) 50 mcg/ml injection solution"
+test_tier = 4
+findClass(test_med,test_tier)
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+test_med = "fentanyl (pf) 500 mcg/ml injection solution"
+test_tier = 4
+findClass(test_med,test_tier)
+
+## ----message=FALSE, warning=FALSE----------------------------------------
+test_med = "fentanyl-acetaminophen 50mg"
+test_tier = 4
+findClass(test_med,test_tier)
 
